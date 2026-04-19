90c84f4e5e233f9a83dec1ea76a6f81e453f7b87
Bharat Joshi <bharatjoshi@spectrayan.com>
2026-04-19 08:40:19 -0500

    chore: remove unused scaffold libraries and stale docs
    Remove placeholder libraries that were replaced by real implementations:
    - libs/frontend/ui (replaced by dsl-renderer components)
    - libs/shared/sdks/angular-sdk (not needed for current arch)
    - libs/shared/sdks/python-sdk (not needed for current arch)
    - libs/shared/types (consolidated into constants lib)
    - docs/prd_gap_analysis.md (outdated analysis)

--- libs/shared/sdks/angular-sdk/src/lib/angular-sdk/angular-sdk.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-angular-sdk',
-  imports: [],
-  templateUrl: './angular-sdk.html',
-  styleUrl: './angular-sdk.scss',
-})
-export class AngularSdk {}
